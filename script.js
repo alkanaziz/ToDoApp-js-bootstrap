1df5fccf11d5f7d91c7eb5d78b9cbb792eab6ca8
alkanaziz <alkanaziz@gmail.com>
2024-01-22 12:11:32 +0100

    tasks-li Element with JS added

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let result;
 
-taskList = document.getElementsByClassName("task")
+let taskList = [
+    {id: 1, taskName: "Task 1", status: "pending"},
+    {id: 2, taskName: "Task 2", status: "pending"},
+    {id: 3, taskName: "Task 3", status: "pending"},
+    {id: 4, taskName: "Task 4", status: "pending"},
+]
+let ulElem = document.getElementById("task-list");
 
-for(let i = 0; i < taskList.length; i++) {
-    console.log((taskList[i]))
-};
 
+for(let task of taskList) {
+    let liElem = document.createElement("li");
+    liElem.className = "task list-group-item list-group-item-info";
+    liElem.innerHTML = /*html*/`
+        <div class="form-check">
+           <input type="checkbox" id="${task.id}" class="form-check-input" />
+           <label for="${task.id}" class="form-check-label">${task.taskName}</label>
+        </div>
+    `;
+ulElem.insertAdjacentElement("beforeend", liElem);
+console.log(liElem)
+}
+console.log(ulElem)
 
-console.log(result)  
