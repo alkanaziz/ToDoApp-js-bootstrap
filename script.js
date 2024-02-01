501e3b5a2f2ba2fea49e82b2aab593a4e74b4b74
alkanaziz <alkanaziz@gmail.com>
2024-02-01 02:09:40 +0100

    displayTask function created

--- script.js
+++ script.js
@@ -3,17 +3,21 @@
 let result;
 
 let tasksList = [
-    {id: 1, taskName: "Task 1", status: "pending"},
-    {id: 2, taskName: "Task 2", status: "pending"},
-    {id: 3, taskName: "Task 3", status: "pending"},
-    {id: 4, taskName: "Task 4", status: "pending"},
+    { id: 1, taskName: "Task 1", status: "pending" },
+    { id: 2, taskName: "Task 2", status: "pending" },
+    { id: 3, taskName: "Task 3", status: "pending" },
+    { id: 4, taskName: "Task 4", status: "pending" },
 ]
-let ulElem = document.getElementById("task-list");
 
-// ADD and DISPLAY TASK from tasksList with "insertAdjacentHTML"
-for(let task of tasksList) {
+displayTasks()
 
-    let liElem = /*html*/ `
+function displayTasks() {
+    let ulElem = document.getElementById("task-list");
+
+    // ADD and DISPLAY TASK from tasksList with "insertAdjacentHTML"
+    for (let task of tasksList) {
+
+        let liElem = /*html*/ `
     <li class="task list-group-item list-group-item-info">
         <div class="form-check">
             <input type="checkbox" id="${task.id}" class="form-check-input" />
@@ -21,8 +25,8 @@ for(let task of tasksList) {
         </div>
     </li>
     `;
-    ulElem.insertAdjacentHTML("beforeend", liElem);
+        ulElem.insertAdjacentHTML("beforeend", liElem);
 
-}
-
+    }
+};
 
